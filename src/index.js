ad7dc44052069179d2b3a1839400b22797c73237
MKCH01 <iammnsh01@gmail.com>
2022-04-30 18:57:25 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// import { Provider } from 'react-redux';
-// import configStore from './store/configStore';
-
-// const store = configStore()
-
-// store.subscribe(() => {
-//   console.log("data", store.getState())
-// })
-
-// //console.log("store", store)
-
-// const jsx = (
-//   <Provider store = {store}>
-//   <App />
-//   </Provider>
-// )
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     jsx,
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 
 import React from "react";
 import ReactDOM from "react-dom";
